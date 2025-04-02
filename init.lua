c1a25451f5e460951989ae25929212cdad208d96
Adrian Padilla Vega (m̵͊͜x̵̛̛͖̍͛͆̌̓̔͒͂͋̄̚͝W̶̧̨̨̯̤̠̣͓͖̱͙͙͑͜ͅỏ̷̱̬͚̞̬̌̎̆̿̔͑͌̔̒̔͛̀͆̚l̵̢̨͔̺͎̭̝̔̅͆f̵͉͔͍̬͕͉͙̹͈̘͈̆̀̿͋̆̅͊̇̾͊́͝) <adrian@adrianpadilla.net>
2025-04-01 19:15:53 -0700

    setting up basic LSP support
    remaining lsp configuration

--- init.lua
+++ init.lua
@@ -1,2 +1,17 @@
 require("config.options") -- Load the config/options.lua module
 require("config.lazy") -- Load the config/lazy.lua module with our plugins using Lazy.vim
+require("config.autocompletion")
+
+ vim.lsp.config('*', {
+  capabilities = {
+    textDocument = {
+      semanticTokens = {
+        multilineTokenSupport = true,
+      }
+    }
+  },
+  root_markers = { '.git' },
+})
+
+-- enables LSP servers
+vim.lsp.enable({'clangd', 'rust-analyzer', 'csharp-ls'})
