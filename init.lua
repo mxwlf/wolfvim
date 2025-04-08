1d177349f507e23e58ff48c4aa756fdf9e7aaa37
Adrian Padilla Vega (m̵͊͜x̵̛̛͖̍͛͆̌̓̔͒͂͋̄̚͝W̶̧̨̨̯̤̠̣͓͖̱͙͙͑͜ͅỏ̷̱̬͚̞̬̌̎̆̿̔͑͌̔̒̔͛̀͆̚l̵̢̨͔̺͎̭̝̔̅͆f̵͉͔͍̬͕͉͙̹͈̘͈̆̀̿͋̆̅͊̇̾͊́͝) <adrian@adrianpadilla.net>
2025-04-08 11:26:49 -0700

    configuring more lsps

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 require("config.options") -- Load the config/options.lua module
 require("config.lazy") -- Load the config/lazy.lua module with our plugins using Lazy.vim
 
+
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.csharp_ls.setup{}
 -- vim.lsp.config('*', {
 --  capabilities = {
 --    textDocument = {
