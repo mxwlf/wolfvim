4c6c05225f4b4f2dff967904faa605d946ad058f
Adrian Padilla Vega (m̵͊͜x̵̛̛͖̍͛͆̌̓̔͒͂͋̄̚͝W̶̧̨̨̯̤̠̣͓͖̱͙͙͑͜ͅỏ̷̱̬͚̞̬̌̎̆̿̔͑͌̔̒̔͛̀͆̚l̵̢̨͔̺͎̭̝̔̅͆f̵͉͔͍̬͕͉͙̹͈̘͈̆̀̿͋̆̅͊̇̾͊́͝) <adrian@adrianpadilla.net>
2025-04-22 23:35:36 -0700

    adding keybindings on lspattach

--- init.lua
+++ init.lua
@@ -2,15 +2,58 @@ require("config.options") -- Load the config/options.lua module
 require("config.lazy") -- Load the config/lazy.lua module with our plugins using Lazy.vim
 
 
-require'lspconfig'.pyright.setup{}
-require'lspconfig'.csharp_ls.setup{}
--- vim.lsp.config('*', {
---  capabilities = {
---    textDocument = {
---      semanticTokens = {
---        multilineTokenSupport = true,
---      }
---    }
---  },
---  root_markers = { '.git' },
---})
+vim.lsp.enable('csharp_ls')
+vim.lsp.config('*', {
+  capabilities = {
+    textDocument = {
+      semanticTokens = {
+        multilineTokenSupport = true,
+      }
+    }
+  },
+  root_markers = { '.git' },
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('my.lsp', {}),
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    local keymap = vim.api.nvim_set_keymap
+	local opts = { noremap = true, silent = true }
+	--buf_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+	keymap("n", "<leader>pw", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+	keymap("n", "<leader>nw", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+	keymap("n", "<leader>pe", "<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.ERROR})<CR>", opts)
+	keymap("n", "<leader>ne", "<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.ERROR})<CR>", opts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+	vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, { buffer = 0 })
+	vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { buffer = 0 })
+	vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+
+    if client:supports_method('textDocument/implementation') then
+      -- Create a keymap for vim.lsp.buf.implementation ...
+
+    end
+    -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+    if client:supports_method('textDocument/completion') then
+      -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+      -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+      -- client.server_capabilities.completionProvider.triggerCharacters = chars
+      vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
+    end
+    -- Auto-format ("lint") on save.
+    -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+    if not client:supports_method('textDocument/willSaveWaitUntil')
+        and client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = vim.api.nvim_create_augroup('my.lsp', {clear=false}),
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+        end,
+      })
+    end
+  end,
+})
